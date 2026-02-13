14cd05e54d183b2f92b890fb82465fccb0d0e305
imerasenberg <imerasenberg@gmail.com>
2026-02-13 11:29:13 +0100

    fixed the FCC generation and started on the documentation in LaTeX editor

--- excersise_4/E4_FCC.c
+++ excersise_4/E4_FCC.c
@@ -7,22 +7,39 @@ int main(){
     float d = 1.0; // the distance between two spheres
     float a = 1.0; // the radius of an sphere
 
+    // creating an distance variable that makes les typing
+    float l = sqrt(2.0)*d;
+
+    // defining the size of the box that will be spanned
+    float x_max = l*N;
+
     // Make a file where we can save the position data
     FILE *print_coords; // inititialises a file variable
     print_coords = fopen("FCC_xyz.dat","w"); // defining the file variable to be the opening of some file cubic.xyz
 
     // Let us print some initial coordinates
-    fprintf(print_coords, "%i\n", N*N*N); // the total number of particles
-    fprintf(print_coords, "%lf\t%lf\n", -0.0, 1.0*d*N); // The ocupied space in the x direction
-    fprintf(print_coords, "%lf\t%lf\n", -0.0, 1.0*d*N); // The ocupied space in the y direction
-    fprintf(print_coords, "%lf\t%lf\n", -0.0, 1.0*d*N); // The ocupied space in the z direction
+    fprintf(print_coords, "%i\n", 4*N*N*N); // the total number of particles
+    fprintf(print_coords, "%lf\t%lf\n", -0.0, x_max); // The ocupied space in the x direction
+    fprintf(print_coords, "%lf\t%lf\n", -0.0, x_max); // The ocupied space in the y direction
+    fprintf(print_coords, "%lf\t%lf\n", -0.0, x_max); // The ocupied space in the z direction
 
     // we first initialise the particle possision saving arrays
-    float x[N*N*N], y[N*N*N], z[N*N*N], r[N*N*N];
-
+    float x[4*N*N*N], y[4*N*N*N], z[4*N*N*N], r[4*N*N*N];
 
     // now we start generating particle possitions and radiuses
     int n = 0; // this is our counting variable, it wil index which particle we will consider
+
+    /* 
+    The latice points are described by
+    R= a_1 n_x + a_2 n_x + a_3 n_z
+    a_1 = a/2 (j + k)
+    a_2 = a/2 (i + k)
+    a_3 = a/2 (i + j)
+    i, j, k are the unit vectors in x, y and z directions respectively (not the counts)
+    */
+
+
+
 
     // sweeping over the N_x particles
     for(int i=0; i<N; i++){
@@ -31,15 +48,55 @@ int main(){
             // sweeping over the N_z particles
             for(int k=0; k<N; k++){
                 // generating the possition for i,j,k latice cite, also the radius of the particle
-                x[n]= (i+0.5)*d;
-                y[n]= (j)+0.5*d; 
-                z[n]= (k+0.5)*d;
+
+                // first we start on the base vector because we know this patern reapeats every 2*unit vector in each direction
+                x[n]= (i)*l;
+                y[n]= (j)*l; 
+                z[n]= (k)*l;
                 r[n]= a;
 
                 // saving the x,y,z possition and radius of the particle
                 fprintf(print_coords, "%lf\t%lf\t%lf\t%lf\n", x[n], y[n],z[n],r[n]);
 
                 n++;
+
+                // here we will add the a_1 vector and make the same spacing
+                x[n]= (i)*l;
+                y[n]= (j+0.5)*l; 
+                z[n]= (k+0.5)*l;
+                r[n]= a;
+
+                // saving the x,y,z possition and radius of the particle
+                fprintf(print_coords, "%lf\t%lf\t%lf\t%lf\n", x[n], y[n],z[n],r[n]);
+
+                n++;
+
+                // here we will add the a_2 vector and make the same spacing
+                x[n]= (i+0.5)*l;
+                y[n]= (j)*l; 
+                z[n]= (k+0.5)*l;
+                r[n]= a;
+
+                // saving the x,y,z possition and radius of the particle
+                fprintf(print_coords, "%lf\t%lf\t%lf\t%lf\n", x[n], y[n],z[n],r[n]);
+
+                n++;
+
+                // here we will add the a3 vector and continue the same spacing
+                x[n]= (i+0.5)*l;
+                y[n]= (j+0.5)*l; 
+                z[n]= (k)*l;
+                r[n]= a;
+
+                // saving the x,y,z possition and radius of the particle
+                fprintf(print_coords, "%lf\t%lf\t%lf\t%lf\n", x[n], y[n],z[n],r[n]);
+
+                n++;
+
+
+
+
+
             }
         }
     }
--- excersise_4/E4_FCC_wrong.c
+++ excersise_4/E4_FCC_wrong.c
@@ -7,31 +7,34 @@ int main(){
     float d = 1.0; // the distance between two spheres
     float a = 1.0; // the radius of an sphere
 
+    float x_max = sqrt(2)*d*N;
+
     // Make a file where we can save the position data
     FILE *print_coords; // inititialises a file variable
-    print_coords = fopen("cubic_xyz.dat","w"); // defining the file variable to be the opening of some file cubic.xyz
+    print_coords = fopen("FCC_xyz.dat","w"); // defining the file variable to be the opening of some file cubic.xyz
 
     // Let us print some initial coordinates
     fprintf(print_coords, "%i\n", N*N*N); // the total number of particles
-    fprintf(print_coords, "%lf\t%lf\n", -0.0, 1.0*d*N); // The ocupied space in the x direction
-    fprintf(print_coords, "%lf\t%lf\n", -0.0, 1.0*d*N); // The ocupied space in the y direction
-    fprintf(print_coords, "%lf\t%lf\n", -0.0, 1.0*d*N); // The ocupied space in the z direction
+    fprintf(print_coords, "%lf\t%lf\n", -0.0, x_max); // The ocupied space in the x direction
+    fprintf(print_coords, "%lf\t%lf\n", -0.0, x_max); // The ocupied space in the y direction
+    fprintf(print_coords, "%lf\t%lf\n", -0.0, x_max); // The ocupied space in the z direction
 
     // we first initialise the particle possision saving arrays
     float x[N*N*N], y[N*N*N], z[N*N*N], r[N*N*N];
-
 
     // now we start generating particle possitions and radiuses
     int n = 0; // this is our counting variable, it wil index which particle we will consider
 
     /* 
     The latice points are described by
-    R= a_x n_x + a_y n_x + a_z n_z
-    a_x = i a 
-    a_y = j a
-    a_z = k a
-    */
+    R= a_1 n_x + a_2 n_x + a_3 n_z
+    a_1 = a/2 (j + k)
+    a_2 = a/2 (i + k)
+    a_3 = a/2 (i + j)
+    i, j, k are the unit vectors in x, y and z directions respectively (not the counts)
 
+    What we will do is first generate the matrix placing all points that fall outside the box at -100, -100, -100
+    */
 
     // sweeping over the N_x particles
     for(int i=0; i<N; i++){
@@ -40,9 +43,9 @@ int main(){
             // sweeping over the N_z particles
             for(int k=0; k<N; k++){
                 // generating the possition for i,j,k latice cite, also the radius of the particle
-                x[n]= (i+0.5)*d;
-                y[n]= (j+0.5)*d; 
-                z[n]= (k+0.5)*d;
+                x[n]= d/sqrt(2.0) * (j + k);
+                y[n]= d/sqrt(2.0) * (i + k); 
+                z[n]= d/sqrt(2.0) * (i + j);
                 r[n]= a;
 
                 // saving the x,y,z possition and radius of the particle
